c8c6c958f64eb1012d70c2995165c2c795f8bd8d
Bogdan Lazar <bogdan@bogdanlazar.com>
2021-02-17 14:40:56 +0100

    Simplify bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,18 @@
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 
-import pkg from './package.json';
+const pkgName = require('./package.json').name;
 
-const pkgName = pkg.name
-  .split('-')
-  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-  .join('');
+const ext = (format) =>
+  format === 'dts' ? 'd.ts' : format === 'cjs' ? 'js' : 'mjs';
 
-function pathForBuild(format, production) {
-  const ext = `${production ? '.min' : ''}.${format === 'dts' ? 'd.ts' : 'js'}`;
-  switch (format) {
-    case 'umd':
-      return `dist/bundles/${pkg.name}${ext}`;
-    case 'es':
-      return `dist/lib-esm/${pkg.name}${ext}`;
-    case 'dts':
-      return `dist/${pkg.name}${ext}`;
-
-    // cjs
-    default:
-      return `dist/lib/${pkg.name}${ext}`;
-  }
-}
-
-function bundle(format, production) {
+function bundle(format, production = false) {
   return {
     input: 'src/index.ts',
     output: {
-      name: pkgName,
-      file: pathForBuild(format, production),
-      format,
-      sourcemap: format !== 'dts',
+      file: `dist/${pkgName}${production ? '.min' : ''}.${ext(format)}`,
+      format: format === 'cjs' ? 'cjs' : 'es',
+      sourcemap: format !== 'dts' && production,
       exports: format === 'cjs' ? 'named' : 'default'
     },
     plugins: format === 'dts' ? [dts()] : [esbuild({ minify: production })],
@@ -43,10 +24,8 @@ const bundles = [
   bundle('dts'),
   bundle('es'),
   bundle('cjs'),
-  bundle('umd'),
   bundle('es', true),
-  bundle('cjs', true),
-  bundle('umd', true)
+  bundle('cjs', true)
 ];
 
 export default bundles;
