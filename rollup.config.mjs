e88913aca79eaa7970b3ba02823b2aaa01763971
Bogdan Lazar <bogdan@bogdanlazar.com>
2023-02-03 10:03:07 +0100

    Remove type and add d.mts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,8 +3,16 @@ import esbuild from 'rollup-plugin-esbuild';
 
 const pkgName = 'highlight-words';
 
+const isTyping = (format) => ['dts', 'dmts'].includes(format);
+
 const ext = (format) =>
-  format === 'dts' ? 'd.ts' : format === 'cjs' ? 'js' : 'mjs';
+  format === 'dts'
+    ? 'd.ts'
+    : format === 'dmts'
+    ? 'd.mts'
+    : format === 'cjs'
+    ? 'js'
+    : 'mjs';
 
 function bundle(format, production = false) {
   return {
@@ -12,16 +20,17 @@ function bundle(format, production = false) {
     output: {
       file: `dist/${pkgName}${production ? '.min' : ''}.${ext(format)}`,
       format: format === 'cjs' ? 'cjs' : 'es',
-      sourcemap: format !== 'dts' && production,
+      sourcemap: !isTyping(format) && production,
       exports: format === 'cjs' ? 'named' : 'default'
     },
-    plugins: format === 'dts' ? [dts()] : [esbuild({ minify: production })],
+    plugins: isTyping(format) ? [dts()] : [esbuild({ minify: production })],
     external: (id) => !/^[./]/.test(id)
   };
 }
 
 const bundles = [
   bundle('dts'),
+  bundle('dmts'),
   bundle('es'),
   bundle('cjs'),
   bundle('es', true),
